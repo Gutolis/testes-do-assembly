16ab7ae900a55178952a1eab16e9cf2ed6f65e4e
Gutolis <padilha.augusto@escola.pr.gov.br>
2025-11-03 21:13:21 -0300

    tentando

--- root.s
+++ root.s
@@ -1,14 +1,1 @@
-.section .text
-.globl _start
-_start:
- xor %rdi,%rdi
- xor %rsi,%rsi
- mov $107,%rax
- syscall
- mov $59,%rax
- lea bash(%rip),%rdi
- xor %rsi,%rsi
- xor %rdx,%rdx
- syscall
-bash:
- .string "/bin/bash"
+404: Not Found
