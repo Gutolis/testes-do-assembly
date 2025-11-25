d38a456c06c3274060e4e7c2303915d207308dd9
Gutolis <padilha.augusto@escola.pr.gov.br>
2025-11-25 19:35:20 -0300

    ok

--- root.s
+++ root.s
@@ -1,15 +1,14 @@
+
 .section .text
 .globl _start
-
 _start:
-    /* setreuid(0,0) */
     xor     %rdi, %rdi
     xor     %rsi, %rsi
-    mov     $107, %rax
+    mov     $-10, %rdx      # -10 = máxima prioridade sem root
+    mov     $140, %rax      # 140 = setpriority (correto!)
     syscall
 
-    /* execve("/bin/bash", NULL, NULL) */
-    mov     $59, %rax
+    mov     $59, %rax       # execve
     lea     path(%rip), %rdi
     xor     %rsi, %rsi
     xor     %rdx, %rdx
